1e74bec1110bf02c5213c8ceaa73102b8a7c8ec2
shukrant <shukrant007@gmail.com>
2017-06-13 23:14:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/airticle-one',function(req,res) {
-   res.sendFile(path.join(__dirname, 'ui', 'index.html')); 
+   res.send("Airticle One"); 
 });
 
 app.get('/airticle-two',function(req,res) {
