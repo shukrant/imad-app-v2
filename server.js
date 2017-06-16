12eaabecc060c647255a3690b82a7cd2a71cdc95
shukrant <shukrant007@gmail.com>
2017-06-16 11:35:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,41 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var airticleone = {
+    title : "Airticle-one : Shukrant tyagi",
+    heading : "Airticle-one",
+    content : `<p>
+                    Files on the sidebar represent the source code of your web app. These files are all actually saved in a git repository on your github account. This console allows you to edit these files, deploy your app, and save these files back to your github repository.
+               </p>`
+    
+};
+
+var htmlTemplate = 
+<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class = "container">
+            <div>
+                <a href="/">Home</a>
+            </div>
+            <hr/>
+            <h3>
+                ${heading}
+            </h3>
+            <div>
+                ${content}
+            </div>
+        </div>
+    </body>
+</html>
+;
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
