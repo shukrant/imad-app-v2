77cf481e8ead2eaa3d9423c48278775aa8ed6750
shukrant <shukrant007@gmail.com>
2017-06-16 23:29:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,82 +6,117 @@ var app = express();
 app.use(morgan('combined'));
 
 var articles = {
-    'article-one':  {
-        title: 'Airticle-one : Shukrant tyagi',
-        heading: 'Airticle-one',
-        content: `<p>
-                    Files on the sidebar represent the source code of your web app. These files are all actually saved in a git repository on your github account. This console allows you to edit these files, deploy your app, and save these files back to your github repository.
-               </p>`
-    
-    },
-    'artitcle-two': {
-        title : "Airticle-two : Shukrant tyagi",
-        heading : "Airticle-two",
-        content : `<p>
-                    Files on the sidebar represent the source code of your web app. These files are all actually saved in a git repository on your github account. This console allows you to edit these files, deploy your app, and save these files back to your github repository.
-               </p>`
-    
-    },
-    'article-third': {
-         title : "Airticle-third : Shukrant tyagi",
-        heading : "Airticle-third",
-        content : `<p>
-                    Files on the sidebar represent the source code of your web app. These files are all actually saved in a git repository on your github account. This console allows you to edit these files, deploy your app, and save these files back to your github repository.
-               </p>`
-    }
+'article-one': {
+title: 'Article one | Shukrant',
+heading:'Article one',
+date: 'February-10-2017',
+content: 
+`<p>
+This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+</p>
+<p>
+This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+</p>
+<p>
+This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+</p>
+<p>
+This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+</p>
+`
+},
+'article-two': {
+title: 'Article two | Krishna',
+heading:'Article Two',
+date: 'February-15-2017',
+content:
+`
+<p>
+This is the content for my second article
+</p>
+`
+},
+'article-three': {
+title: 'Article Three | Krishna',
+heading:'Article Three',
+date: 'February-25-2017',
+content:
+`
+<p>
+This is the content for my Third article
+</p>
+`
+}
 };
+function createTemplate (data){
+var date = data.date;
+var content = data.content;
+var heading = data.heading;
+var title = data.title;
 
-function createTemplate(data) {
-    var title = data.title;
-    var heading = data.heading;
-    var content = data.content;
-    var htmlTemplate = `
-    <html>
-        <head>
-            <title>
-                ${title}
-            </title>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <link href="/ui/style.css" rel="stylesheet" />
-        </head>
-        <body>
-            <div class = "container">
-                <div>
-                    <a href="/">Home</a>
-                </div>
-                <hr/>
-                <h3>
-                    ${heading}
-                </h3>
-                <div>
-                    ${content}
-                </div>
-            </div>
-        </body>
-    </html>
-    `;
-    return htmlTemplate;
+var htmlTemplate =  `
+
+<html>
+<head>
+    <title>
+        ${title}
+        
+        
+    </title>
+    <meta name="viewport" content="width=device-width,intial-scale=1"/>
+     <link href="/ui/style.css" rel="stylesheet" />
+     
+</head>
+<body>
+    <div class="container">
+        
+    <div>
+        <a href="/">Home</a>
+        
+    </div>
+    <hr/>
+    <h3>
+        ${heading}
+        
+    </h3>
+    <div>
+        ${date}
+        
+    </div>
+    <div>
+        
+        ${content}
+    </div>
+    </div>
+</body>
+</html>
+
+`;
+return htmlTemplate;
 }
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/:articleName',function(req,res) {
-    var articleName = req.params.articleName; 
-   res.send(createTemplate(articles[articleName]));
+app.get('/:articleName', function (req,res){
+var articleName = req.params.articleName;
+res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+var port = 8080; 
 
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
